c0ce916e06e7e9198383f66a10f8f7a511a0a7ec
tudes <armand.dan26@gmail.com>
2025-07-01 13:53:55 +0200

    fix

--- src/components/commands/neofetch.tsx
+++ src/components/commands/neofetch.tsx
@@ -6,20 +6,35 @@ export default function NeoFetchCommand({
   onLoadEnd?: () => void;
 }): JSX.Element {
   const [fileText, setfileText] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     fetch("/files/neofetch.txt")
       .then((res) => res.text())
       .then((text) => {
         setfileText(text);
+        setLoading(false);
         setTimeout(() => {
           onLoadEnd?.();
         }, 30);
       })
       .catch(() => {
-        setfileText("Error loading neofetch information.");
+        setLoading(false);
       });
   }, [onLoadEnd]);
+
+  if (fileText !== null) {
+    return (
+      <div
+        className="terminal-html"
+        dangerouslySetInnerHTML={{ __html: fileText }}
+      />
+    );
+  }
+
+  if (!loading) {
+    return <span>Error when loading neofetch file</span>;
+  }
 
   if (fileText !== null) {
     return (

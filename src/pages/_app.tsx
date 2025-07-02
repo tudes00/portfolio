2d5fa72f8127905dd517ea9d8b73907d58c8bbe1
tudes <armand.dan26@gmail.com>
2025-07-02 16:20:07 +0200

    test fukcing nbr 5556

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Tudes</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta charSet="UTF-8"/>
         <meta name="description" content="Tudes Portfolio"/>
         <meta name="keywords" content="HTML, CSS, JavaScript"/>

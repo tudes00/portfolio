f8e97d6f0cf91a0059f465438f0faefcd656bbd2
tudes <armand.dan26@gmail.com>
2025-07-04 11:02:40 +0200

    added already boot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AsciiLayout from "../components/asciiLayout";
 import Terminal from "../components/terminal";
 import BootScreen from "../components/bootScreen";
@@ -6,8 +6,18 @@ import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   const [bootFinished, setBootFinished] = useState(
-    process.env.NODE_ENV === "development",
+    //process.env.NODE_ENV === "development",
+    false
   );
+  useEffect(() => {
+    if (window.sessionStorage.getItem("alreadyBoot")) {
+    console.log("alreadyBoot");
+    setBootFinished(true);
+  } else {
+    window.sessionStorage.setItem("alreadyBoot", "true");
+  }
+  }, []);
+  
 
   return (
     <AsciiLayout>
